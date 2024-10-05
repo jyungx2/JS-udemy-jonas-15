5af09a0907f51b498e2929827a8114a7af96ec19
jiyoungnim <cloundyon31@gmail.com>
2024-10-05 23:56:42 +0900

    10/5 (Sat) Section 15) 240

--- starter/script.js
+++ starter/script.js
@@ -68,35 +68,21 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 // 📌 238. Refactoring for Project Architecture
 class App {
-  // 3️⃣ global EC에 선언한 mapEvent, map을 모두 App class로 만든 객체 안에 프라이빗하게 넣어 외부에서 맘대로 조작할 수 없도록 하자 => private field로 만들자.
-  // ⛳️ loadMap메서드 안에서 map(Event) -> this.#map으로 바꿔주자. (더 이상 글로벌 스콥에 있지 않고, 객체 안에 생성되는 것이므로 this keyword 적어줘야함!)
   #mapEvent;
   #map;
+  #workouts = [];
 
   constructor() {
-    // 2️⃣ global EC에 쓰지 말고, constructor안에 넣자!💫
-    //  이 contstructor 메서드는 새로운 객체가 만들어질 때 즉시 호출된다. 즉 페이지가 로드되자마자 객체가 만들어지기 때문에 함께 실행되는 이 constructor 메서드도 페이지가 로드되자마자 호출된다.
-    // Load page -> constructor() -> _getPosition() ... trigger되는 순서
+    // this.workouts = [];
+
     this._getPosition();
 
-    // 📌 236. Rendering workout inform form
-    // form.addEventListener + inputType.addEventListener => 이 둘은 모두 객체 안에서 유저가 해당 이벤트 발생시에 실행되야 하는 코드가 담겨 있음 ->  객체 안에 존재해야 하는 이벤트 리스너 함수..
-    // 5️⃣ _newWorkout() 메서드에 기존 함수 넣기
     form.addEventListener('submit', this._newWorkout.bind(this));
-    // 💥this keyword = DOM element onto which object it is attached = form element => app object를 가리키지 않는다!💥
-    // 이러한 부분들은 우리가 클래스 내에서 이벤트리스너의 this keyword를 다룰 때 항상 조심해야 하는 부분이다!
-    // 사실, 실무에서는 클래스내에서 이벤트리스너를 다룰 땐 항상 this 키워드 상에서 bind method를 사용해야할 것..🌟 명심하자!!!!!
 
-    // 6️⃣ _toggleElevationField() 메서드에 기존 함수 넣기
-    // 이 함수 내에선 this keyword 사용하지 않기 때문에 bind method사용할 필요 ❌
     inputType.addEventListener('change', this._toggleElevationField);
   }
 
-  // 1️⃣ getPosition 메서드에 우리가 이미 작성해놓은 코드 붙이기 + 첫번째 매개변수에 대한 함수의 바디를 loadMap 메서드에 따로 붙인 후, bind method를 활용해 연결하기.
   _getPosition() {
-    // 📌 233. How to use geolocation API? (=internalization, timer API in the browser)
-    // But this is the ✨modern one✨ and very easy to use!!
-    // 파라미터로 2개의 콜백함수를 받는데, 하나는 유저의 현재위치를 획득하는데 성공했을 때 콜되는 함수, 다른 하나는 데이터획득에 실패했을 때 실행되는 함수이다.
     if (navigator.geolocation)
       navigator.geolocation.getCurrentPosition(
         this._loadMap.bind(this), // ✅4️⃣ bind method로 this -> object 가리키도록 하여 해결
@@ -106,31 +92,20 @@ class App {
       );
   }
 
-  // 1️⃣ 여기까지만 하면 아무일도 안일어난다. 단지 계획만 세운것일뿐! 이것들이 실제로 동작할 수 있게 실제 오브젝트를 만든 후, getPosition()를 불러와야 한다.
   _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-    console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+    // 💫 GeolocationPosition 객체 안의 coords Property 이용
+    // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
 
     const coords = [latitude, longitude];
 
-    // 1. map function의 파라미터: must be the ID name of an element in our HTML.
-    // 2. L: Leaflet이 entry point로서 제공하는 main function (L namespace은 우리가 사용가능한 몇개의 메서드를 가지고, 그 중에 하나가 map, titleLayer, marker method인 것)
-    // 3. map - setView의 두번째 매개변수: 얼마나 맵이 확대되어 나타나는지의 정도
-
-    console.log(this); // 🚨4️⃣ undefined (In a regular function call, this keyword is set to undefined)
-    // this._loadMap: 우리가 직접 부르는 함수가 아닌, getCurrentPosition()함수에 의해 불려진 콜백함수로서, 이 함수 자체는 method call이 아닌, regular function call으로 취급되기 때문에 undefined.
-    // ✅ this 키워드를 명시적으로 정해주는 함수 메서드 배웠다. - 근데 여기선 함수를 불러오는 것(Call, apply)이 아닌, 함수 자체를 리턴하고 싶기 때문에 "bind"메서드를 이용해야겠지.
-    // 10-Functions........................
-    // ⭐️ bind: call method처럼 모든 함수의 this keyword를 set할 수 있게 allow하지만, call과의 차이점은 bind는 call처럼 바로 불러오지 않고 '새로운 함수를 return함으로써' 새로운 normal function을 만든 효과 -> parameter만 넣어 작용⭐️시킬 수 있다.
+    console.log(this);
+    console.log(position);
 
     // 235. Displaying a Map Marker
     this.#map = L.map('map').setView(coords, 13); // ⛳️
-    // map 변수 상에서 이벤트리스너를 불러올 수 있기 때문에 저장한 거였다.
-    // map은 leaflet 라이브러리로부터 생성된 특별한 객체이고, 이 객체 상에서 on 속성을 불러올 수 있다. (자바스크립트로부터 온 속성이 아니다!)
-    // console.log(map); // 이 안에 'on' property가 존재하는 것을 확인할 수 있다.✅
 
-    // org -> fr/hot 변경하여 맵의 테마를 변경
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -138,8 +113,6 @@ class App {
 
     // Handling clicks on map
     // 7️⃣ _showform()
-    // 💥 this keyword -> bind method이용하여 제대로 된 this 키워드를 명시해주자...💥
-    // 여기서 this = #map이기 때문에(on = addEventlistener) app obejct를 가리키도록 bind(this)해주자.
     this.#map.on('click', this._showForm.bind(this)); // ⛳️
   }
 
@@ -159,35 +132,69 @@ class App {
 
   // 5️⃣ _newWorkout() 메서드에 기존 함수 넣기
   _newWorkout(e) {
+    // 🤩 every method를 이용한 checking valid numbers 🤩
+    // 1. If all the inputs are real numbers
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp)); // 배열 내 모든 요소가 조건을 만족한다면 true 반환, 하나라도 만족하지 않으면 false (some은 하나라도 만족하면 true )
+
+    // 2. If all the inputs  are positive
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
     e.preventDefault();
 
     // 1) Get data from form
     const type = inputType.value; // running or cycling
     const distance = +inputDistance.value; // always comes into a string, so have to convert it to number immediately.
     const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng; // ⛱️ 오브젝트 생성 시 필요한 값들.. 3번 코드 위로 위치 이동!
+    let workout;
 
     // 2) Check if data is valid
 
     // 3) If activity running, create running object
+    // if-else절은 앞으로 많이 보지 못할 것. 가독성이 떨어지고 코드가 지저분해보인다.
+    // 그냥 다음과 같이 if()절을 따로따로 하나씩 나눠 쓰자.
     if (type === 'running') {
-      const cadence = +inputCadence.value;
-      // Check if data is valid
-      // ✅ This was your go-to whenever you need to "check if st is a number or not".
-      if (!Number.isFinite(distance))
+      const cadence = +inputCadence.value; // +: string => number
+      // ✅ Check if data is valid
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
         return alert('Inputs have to be positive numbers!');
+      // 👉 Number.isFinite(): This was your go-to whenever you need to "check if st is a number or not".
+      // 👉 Guard Clause: check for the opposite of what we're originally interested in and if that opposite is true, then we simply return the function immediately. => trait of more modern JS. kind of trend in modern JS..
+
+      // Creating a new object
+      workout = new Running([lat, lng], distance, duration, cadence);
+      //  this.#workouts.push(workout); // 👉 5)으로 이동
     }
 
     // 4) If activity cycling, create cycling object
     if (type === 'cycling') {
       const elevation = +inputElevation.value;
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to be positive numbers!');
+
+      // Creating a new object
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+      //  this.#workouts.push(workout); // 👉 5)으로 이동
     }
 
     // 5) Add new object to workout array
+    this.#workouts.push(workout); // 👉 3)과 4)에서 공통으로 필요한 코드
 
     // 6) Render workout on map as marker
     //  📌 234. How to display a map using a third party library called Leaflet.
     // map event안에 있는 latlng 객체 안의 lat, lng 프라퍼티 도출
-    const { lat, lng } = this.#mapEvent.latlng;
+    // const { lat, lng } = this.#mapEvent.latlng; // ⛱️ 맨 위로 옮기기
+
     // L.marker~ : map 상에 클릭할 때마다 Marker를 표시하는 코드
     // coords = 이전에 우리가 geolocation으로부터 명시한 map의 센터 -> [lat, lng] 삽입
     L.marker([lat, lng])
@@ -197,14 +204,13 @@ class App {
           maxWidth: 250,
           minWidth: 100,
           autoClose: false, // 다른 팝업을 생성하려고 클릭했을 때 현재 팝업이 자동으로 닫히는 것을 방지함.
-          // closeOnClick: false, // 유저가 팝업 이외에 "다른 부분"을 클릭했을 때 지워지는 것을 방지 => 항상 팝업이 떠있도록 false로 설정!
+          closeOnClick: false, // 유저가 팝업 이외에 "다른 부분"을 클릭했을 때 지워지는 것을 방지 => 항상 팝업이 떠있도록 false로 설정!
           className: 'running-popup', // will define this dynamically sometime.
         })
       )
       .setPopupContent('Workout') // string이나 html element를 넣을 수 있음.
       // 이 메서드는 대부분의 메서드와 더불어 this키워드를 리턴하므로 메서드 사용이 다음과 같이 chainable하다.
       .openPopup();
-    // bindPopup안에 여러 옵션을 포함하는 객체를 넣을 수도 있다.
 
     // 7) Hide form + clear input fields
     inputDistance.value =
--- starter/typing.js
+++ starter/typing.js
@@ -13,22 +13,38 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class App {
   // global scope에 정의내린 변수들을 App class안으로 가져와 최대한 연결되도록..(수업 때 언급됐던 결합도 높인다는 개념?)
-  // 우리는 앱과 관련된 모든 프라퍼티와 메서드를 외부에서 마음대로 조작할 수 없도록 Private하게 유지하고 싶기 때문에 # 붙인다. => 메서드에 존재하는 모든 map, mapEvent에 #을 붙여줘야 한다. => 💥이렇게 this keyword를 사용시, 첫번째 에러 발생💥
+  // 3️⃣ global EC에 선언한 mapEvent, map을 모두 App class로 만든 객체 안에 프라이빗하게 넣어 외부에서 맘대로 조작할 수 없도록 하자 => private field로 만들자.
+  // ⛳️ loadMap메서드 안에서 map(Event) -> this.#map으로 바꿔주자. (더 이상 글로벌 스콥에 있지 않고, 객체 안에 생성되는 것이므로 this keyword 적어줘야함!)
   #map;
   #mapEvent;
 
   constructor() {
+    // 2️⃣ global EC에 쓰지 말고, constructor안에 넣자!💫
+    //  이 contstructor 메서드는 새로운 객체가 만들어질 때 즉시 호출된다. 즉 페이지가 로드되자마자 객체가 만들어지기 때문에 함께 실행되는 이 constructor 메서드도 페이지가 로드되자마자 호출된다.
+    // Load page -> constructor() -> _getPosition() ... trigger되는 순서
     this._getPosition(); // ✨
 
+    // 📌 236. Rendering workout inform form
+    // form.addEventListener + inputType.addEventListener => 이 둘은 모두 객체 안에서 유저가 해당 이벤트 발생시에 실행되야 하는 코드가 담겨 있음 ->  객체 안에 존재해야 하는 이벤트 리스너 함수..
+    // 5️⃣ _newWorkout() 메서드에 기존 함수 넣기
     // 스크립트가 로드되자마자 만들어져야 코드 -> 유저가 다음과 같은 이벤트를 발생시킬 때마다 실행되어야 하는 코드이기 때문에,  constructor 메서드 안에..
     form.addEventListener('submit', this._newWorkout.bind(this));
     // 🌟 폼을 제출한다는 것은 새로운 운동기록을 만든다는 뜻이니까, 콜백함수 부분을 _newWorkout 메서드에 넣고 여기선 그냥 불러오기만... (this._newWorkout)
+
     // 💥2️⃣ eventlistener상에서 this = form 을 가리키므로, bind(this)로 this keyword를 현재 생성된 객체(const app = new App())로 명시해줘야 한다!!
+    // this keyword = DOM element onto which object it is attached = form element => app object를 가리키지 않는다!
+    // 이러한 부분들은 우리가 클래스 내에서 이벤트리스너의 this keyword를 다룰 때 항상 조심해야 하는 부분이다!
+    // 사실, 실무에서는 클래스내에서 이벤트리스너를 다룰 땐 항상 this 키워드 상에서 bind method를 사용해야할 것..🌟 명심하자!!!!!
 
     inputType.addEventListener('change', this._toggleElevationField);
-    // 👌4️⃣ 이 함수 안에서는 this keyword쓰지 않으므로, bind method 필요없다!
+    // 6️⃣ _toggleElevationField() 메서드에 기존 함수 넣기
+    // 이 함수 내에선 this keyword 사용하지 않기 때문에 bind method사용할 필요 ❌
   }
 
+  // 1️⃣ getPosition 메서드에 우리가 이미 작성해놓은 코드 붙이기 + 첫번째 매개변수에 대한 함수의 바디를 loadMap 메서드에 따로 붙인 후, bind method를 활용해 연결하기.
+  // 📌 233. How to use geolocation API? (=internalization, timer API in the browser)
+  // But this is the ✨modern one✨ and very easy to use!!
+  // 파라미터로 2개의 콜백함수를 받는데, 하나는 유저의 현재위치를 획득하는데 성공했을 때 콜되는 함수, 다른 하나는 데이터획득에 실패했을 때 실행되는 함수이다.
   _getPosition() {
     // Getting the user's current position using geolocation API
     if (navigator.geolocation)
@@ -40,6 +56,7 @@ class App {
       );
   }
 
+  // 1️⃣ 여기까지만 하면 아무일도 안일어난다. 단지 계획만 세운것일뿐! 이것들이 실제로 동작할 수 있게 실제 오브젝트를 만든 후, getPosition()를 불러와야 한다.
   // just a blueprint of a house until we make a class that creates objects holding the user's data from inputs.
   _loadMap(position) {
     const { latitude } = position.coords;
@@ -49,28 +66,46 @@ class App {
     const coords = [latitude, longitude];
 
     console.log(this);
+    // 🚨4️⃣ undefined (In a regular function call, this keyword is set to undefined)
+    // this._loadMap: 우리가 직접 부르는 함수가 아닌, getCurrentPosition()함수에 의해 불려진 콜백함수로서, 이 함수 자체는 method call이 아닌, regular function call으로 취급되기 때문에 undefined.
+    // ✅ this 키워드를 명시적으로 정해주는 함수 메서드 배웠다. - 근데 여기선 함수를 불러오는 것(Call, apply)이 아닌, 함수 자체를 리턴하고 싶기 때문에 "bind"메서드를 이용해야겠지.
+    // 10-Functions........................
+    // ⭐️ bind: call method처럼 모든 함수의 this keyword를 set할 수 있게 allow하지만, call과의 차이점은 bind는 call처럼 바로 불러오지 않고 '새로운 함수를 return함으로써' 새로운 normal function을 만든 효과 -> parameter만 넣어 작용⭐️시킬 수 있다.
+
+    // 235. Displaying a Map Marker
     // 밖에서 미리 선언해준 map 배리어블을 최초로 정의해줌!! (값 등록)
     // * L.map()의 파라미터에는 map이 그려질 html 요소의 id값과 동일해야 한다.
-    this.#map = L.map('map').setView(coords, 13); // 💥1️⃣ 첫번째 에러 발생
-    // this상에 #map 프라퍼티 찾을 수 없다는 에러.
+    this.#map = L.map('map').setView(coords, 13); // 💥1️⃣ 첫번째 에러 발생 // ⛳️
+    // 1. map function의 파라미터: must be the ID name of an element in our HTML.
+    // 2. L: Leaflet이 entry point로서 제공하는 main function (L namespace은 우리가 사용가능한 몇개의 메서드를 가지고, 그 중에 하나가 map, titleLayer, marker method인 것)
+    // 3. setView의 두번째 매개변수: 얼마나 맵이 확대되어 나타나는지의 정도
+
+    // 💥1️⃣ this상에 #map 프라퍼티 찾을 수 없다는 에러.
     // this._loadMap : Regular function call (Not a method call Cause we're not calling it ourselves)
     // Regular function call에서 This는 undefined라고 배웠기 때문에 여기서 This = undefined, undefined상에서 #map프라퍼티를 찾는 꼴이 됐으니 에러가 난 것!
     // 👉 bind method 사용하여 this keyword를 명시적으로 지정해주면 해결가능!😍
-    // console.log(map);
 
+    // org -> fr/hot 변경하여 맵의 테마를 변경
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
+    // console.log(map);
+    // map 변수 상에서 이벤트리스너를 불러올 수 있기 때문에 저장한 거였다.
+    // map은 "leaflet 라이브러리로부터 생성된 특별한 객체"이고, 이 객체 상에서 on 속성을 불러올 수 있다. (자바스크립트로부터 온 속성이 아니다!)
+    // console.log(map); // 이 안에 'on' property가 존재하는 것을 확인할 수 있다.✅
+
+    // 7️⃣ _showform()
     // Handling clicks on map
-    this.#map.on('click', this._showForm.bind(this)); // // 💥3️⃣ this keyword will be set to the object onto which the event handler is attached.
-    // => 여기선 map Event상에 On 이벤트리스너를 붙였기 때문에, this -> map을 가리키게 된다.
-    // ✅ 해결방법: bind method 사용
+    this.#map.on('click', this._showForm.bind(this)); // ⛳️ //💥3️⃣ this keyword  will be set to the object onto which the event handler is attached.
+    // 💥 this keyword -> bind method이용하여 제대로 된 this 키워드를 명시해주자...💥
+    // 여기서 this = #map이기 때문에(on = addEventlistener) app obejct를 가리키도록 bind(this)해주자.
   }
 
+  // 7️⃣ _showform()
   _showForm(mapE) {
-    this.#mapEvent = mapE;
+    this.#mapEvent = mapE; // ⛳️
     form.classList.remove('hidden');
     inputDistance.focus();
   }
